cd3a0e1c8de4854f482f3b4e5a0280b9e1aa7ee5
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2019-08-27 15:02:51 -0600

    changes to bg images for about.html, about.js and about.less

--- JavaScript/about.js
+++ JavaScript/about.js
@@ -1,33 +1,29 @@
 $(document).ready(function () {
-    // //     $("slide-image:gt(0)").hide();
 
-    // // setInterval(function() { 
-    // //   $('.slide-image:first')
-    // //     .fadeOut(1000)
-    // //     .next()
-    // //     .fadeIn(1000)
-    // //     .end()
-    // //     .appendTo('.slide-image');
-    // // },  12000);
+        var slideIndex = 0;
+        showSlides();
 
-
-    // console.log("Hello, I am working")
-    // });
-
-    var slideIndex = 0;
-    showSlides();
-
-    function showSlides() {
-        var i;
-        var slides = document.getElementsByClassName("slide-image");
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
+        function showSlides() {
+            var i;
+            var slides = document.getElementsByClassName("slide-image");
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            slideIndex++;
+            if (slideIndex > slides.length) {
+                slideIndex = 1
+            }
+            slides[slideIndex - 1].style.display = "block";
+            setTimeout(showSlides, 10000); // Change image every 10 seconds
         }
-        slideIndex++;
-        if (slideIndex > slides.length) {
-            slideIndex = 1
-        }
-        slides[slideIndex - 1].style.display = "block";
-        setTimeout(showSlides, 2000); // Change image every 2 seconds
-    }
+
+
+    // })
+    // function test() {
+    //     $("img.slide-image").each(function (index) {
+    //         $(this).hide();
+    //         $(this).delay(3000 * index).fadeIn(6000).fadeOut();
+    //     });
+    // }
+    // test();
 })
