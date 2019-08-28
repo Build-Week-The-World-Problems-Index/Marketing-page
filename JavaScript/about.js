259452168c68d379c587dbb5cb08e22eae687d3a
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2019-08-28 10:34:28 -0600

    Made ui changes to team page and lionked to github handles

---
+++ JavaScript/about.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+
+        var slideIndex = 0;
+        showSlides();
+
+        function showSlides() {
+            var i;
+            var slides = document.getElementsByClassName("slide-image");
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            slideIndex++;
+            if (slideIndex > slides.length) {
+                slideIndex = 1
+            }
+            
+            slides[slideIndex - 1].style.display = "block";
+            setTimeout(showSlides, 10000); // Change image every 10 seconds
+        }
+
+
+    // })
+    // function test() {
+    //     $("img.slide-image").each(function (index) {
+    //         $(this).hide();
+    //         $(this).delay(3000 * index).fadeIn(6000).fadeOut();
+    //     });
+    // }
+    // test();
+})
